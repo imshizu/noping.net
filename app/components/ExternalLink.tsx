85f0c109a72e1bfff863d0bc47a7f522556c2d3e
shizu <mikkelelvers@outlook.com>
2024-10-20 13:59:00 +0200

    Inital commit

---
+++ app/components/ExternalLink.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { ReactNode } from "react";
+import { UrlObject } from "url";
+
+interface Props {
+    className?: string;
+    children: ReactNode;
+    href: string | UrlObject;
+    title?: string;
+}
+
+export default function ExternalLink({ className, children, href, title }: Props) {
+    return (
+        <Link
+            className={className ?? "underline underline-offset-4 hover:text-blue-500"}
+            href={href}
+            rel="noopener noreferrer"
+            target="_blank"
+            title={title}
+        >
+            {children}
+        </Link>
+    );
+}
